2c0fdb6851f843d56bc94a029ec023d07d5e8f62
Daniel Murphy <danielmichaelmurphy@gmail.com>
2020-07-28 15:14:54 -0400

    Added bubble-sort folder with README.md and bubble_sort.rb files

---
+++ bubble-sort/README.md
@@ -0,0 +1,32 @@
+Project: Bubble Sort
+
+(https://www.theodinproject.com/courses/ruby-programming/lessons/bubble-sort)
+
+*Problem:*
+
+Write a program that takes in an array of numbers and returns a sorted array. Each element must be compared to the element on its right and if the left element is larger, swap them. Account for how many passes are required for worst case scenarios and take steps to improve efficiency.
+
+*Starting Pseudo Code:* 
+
+# Define a method bubble_sort with parameter array
+# Create a variable n equal to the array's length
+# Loop through array for 1 to n-1 times
+# If element is greater than the next element...
+    # Swap the two elements
+    # Indicate swap
+# End loop once everything is swapped
+# Return sorted array
+
+#Final Pseudo Code:*
+
+# Define a method bubble_sort with parameter array
+# Create a variable n equal to the array's length
+# Loop until array's length (n) is less than or equal to 1
+    # Make new length variable
+    # Loop through array for 1 to n-1 times
+        # If previous element is greater than the next element...
+            # Swap the elements
+            # Assign new length to current iteration number
+    # Assign length to the new length
+# Return sorted array
+
---
+++ bubble-sort/bubble_sort.rb
@@ -0,0 +1,40 @@
+# Define a method bubble_sort with parameter array
+# Create a variable n equal to the array's length
+# Loop until array's length (n) is less than or equal to 1
+    # Make new length variable
+    # Loop through array for 1 to n-1 times
+        # If previous element is greater than the next element...
+            # Swap the elements
+            # Assign new length to current iteration number
+    # Assign length to the new length
+# Return sorted array
+
+# Define a method bubble_sort with parameter array
+def bubble_sort(array)
+  # Create a variable n equal to the array's length
+  n = array.length
+  # Loop until array's length (n) is less than or equal to 1
+  until n <= 1
+    # Make new length variable
+    new_n = 0
+    # Loop through array for 1 to n-1 times
+    for i in 1..(n-1) do
+      # If previous element is greater than the next element...
+      if array[i-1] > array[i]
+        # Swap the elements
+        array[i-1], array[i] = array[i], array[i-1]
+        # Assign new length to current iteration number
+        new_n = i
+      end
+    end
+    # Assign length to the new length
+    n = new_n
+  end
+  # Return sorted array
+  p array
+end
+
+bubble_sort([4,3,78,2,0,2])
+# Project example => [0, 2, 2, 3, 4, 78]
+bubble_sort([9, 8, 6, 4, 2, 1])
+# Worst case scnario => [1, 2, 4, 6, 8, 9]
